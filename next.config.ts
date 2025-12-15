91f7d53085aca120e142931ae99aa19d18e73e65
onurvarsak <onurvarsak@outlook.com>
2025-12-15 18:33:01 +0300

    a

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,8 @@ import type { NextConfig } from "next"
 const nextConfig: NextConfig = {
   output: "export",
   basePath: "/asl",
-  assetPrefix: "/asl/"
+  assetPrefix: "/asl/",
+  images: { unoptimized: true } // GitHub Pages için önemli
 }
 
 export default nextConfig
